d4f9f547bf70394f4e53aeda3215065a9fae6d4d
Josh Matz <josh.matz@gmail.com>
2013-07-09 19:33:21 -0500

    Remove node.js, add jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,98 @@
 module.exports = function(grunt) {
 
-	// Some NPM tasks that we're gonna use...
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-
-	// And let's configure those tasks
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		compass: {
-			production: {
+	    // Metadata.
+	    pkg: grunt.file.readJSON('package.json'),
+	    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+	    // Task configuration.
+	    concat: {
+			options: {
+				banner: '<%= banner %>',
+				stripBanners: true
+			},
+			dist: {
+				src: ['assets/js/<%= pkg.name %>.js'],
+				dest: 'public/js/<%= pkg.name %>.js'
+			}
+	    },
+	    uglify: {
+			options: {
+				banner: '<%= banner %>'
+			},
+			dist: {
+				src: '<%= concat.dist.dest %>',
+				dest: 'public/js/<%= pkg.name %>.min.js'
+			}
+	    },
+	    jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				unused: true,
+				boss: true,
+				eqnull: true,
+				browser: true,
+				globals: {}
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib_test: {
+				src: ['assets/js/**/*.js', 'test/**/*.js']
+			}
+	    },
+	    compass: {
+	        dist: {                   // Target
+	        options: {              // Target options
+	          sassDir: 'assets/styles',
+	          cssDir: 'public/styles',
+	          environment: 'development'
+	        }
+	      },
+	    },
+	    watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: ['jshint:gruntfile']
+			},
+			lib_test: {
+				files: '<%= jshint.lib_test.src %>',
+				tasks: ['jshint:lib_test', 'qunit']
+			},
+			styles: {
+				files: 'assets/styles/**/*.scss',
+				tasks: ['compass'],
 				options: {
-					sassDir: 'assets/styles',
-					imagesDir: 'assets/img',
-					fontsDir: 'assets/fonts',
-					cssDir: 'assets/styles',
-					noLineComments: true,
-					outputStyle: 'compact'
+					livereload: true
+				}
+			},
+			html: {
+				files: 'views/**/*.ejs',
+				options: {
+					livereload: true
 				}
 			}
-		},
-		uglify: {
-			production: {
-				options: {
-					preserveComments: 'some'
-				},
-				files: [
-					{
-						expand: true,
-						cwd: 'assets/',
-						src: ['js/**/*.js'],
-						dest: 'js/',
-						ext: '.min.js'
-					}
-				]
-			}
-		},
-		copy: {
-			production: {
-				files: [
-					{ expand: true, cwd: 'assets/', src: ['fonts/**/*'], dest: '_production/assets/' },
-				]
-			}
 		}
-	});	
+	});
 
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
 
-	// And then group our tasks together
-	grunt.registerTask('default', ['compass', 'uglify', 'copy']);
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'compass']);
 
 };
