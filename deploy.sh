196987bafd50b752b91c730734a9ffff9d540784
Josh Matz <josh.matz@gmail.com>
2016-04-08 17:30:30 -0500

    rebuilding site Fri Apr  8 17:30:29 CDT 2016

--- deploy.sh
+++ deploy.sh
@@ -3,33 +3,38 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+
 echo -e "\033[0;32mClean started...\033[0m"
-# Clear our previous build of any unpublished content
 cd public
+git checkout master
 git rm -rf *
 git commit -a -m "Cleaning before build"
 cd ..
-
 echo -e "\033[0;32mClean complete...\033[0m"
 
-echo -e "\033[0;32mBuild started...\033[0m"
-# Build the project
-# hugo
 
+echo -e "\033[0;32mBuild started...\033[0m"
+hugo
 echo -e "\033[0;32mBuild complete...\033[0m"
 
-# # Add the changes
-# cd public
-# git add .
 
-# # Commit changes.
-# msg="rebuilding site `date`"
-# if [ $# -eq 1 ]
-#   then msg="$1"
-# fi
-# git commit -m "$msg"
+echo -e "\033[0;32mCommiting build to gh-pages...\033[0m"
+cd public
+git add .
 
-# Push source and build repos.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
 git push origin master
+cd ..
+echo -e "\033[0;32mBuild committed...\033[0m"
 
-# Come Back
+
+echo -e "\033[0;32mCommitting submodule changes...\033[0m"
+git add .
+git commit -m "$msg"
+git push origin master
+echo -e "\033[0;32mSubmodule updated...\033[0m"
